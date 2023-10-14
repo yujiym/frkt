48fbc5fee29a20c77d6bafb6a850dd10ff6c31e3
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-10-14 17:52:52 +0900

    init widget

--- apps/widget/app/root.tsx
+++ apps/widget/app/root.tsx
@@ -1,5 +1,5 @@
+import type { LinksFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
