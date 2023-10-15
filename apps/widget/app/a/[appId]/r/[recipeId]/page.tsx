50f0b7d778c353d41325690bad6f3e621eeffb1f
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-10-15 15:02:03 +0900

    update

--- apps/widget/app/a/[appId]/r/[recipeId]/page.tsx
+++ apps/widget/app/a/[appId]/r/[recipeId]/page.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import { HOST } from '@@/lib/const'
 import Logo from '@@/components/svgs/Logo'
 
@@ -10,18 +10,16 @@ export default function Widget() {
     <div className="container mx-auto max-w-xs max-h-[50vh] min-h-fit bg-red-200 text-red-700 relative">
       <div className="px-6 pt-8 pb-20">
         <h1 className="font-bold text-2xl text-center pb-6">NFT widget</h1>
-        <p>Description here.....</p>
-        {/* <h1>app: {appId}</h1>
-              <h1>recipe: {recipeId}</h1> */}
-        {true ? (
-          <div className="my-24 w-full flex justify-center">
-            <div className="loader-sq" />
-          </div>
-        ) : (
+        <p>Description here.....</p>(
+        <div className="my-24 w-full flex justify-center">
+          <div className="loader-sq" />
+        </div>
+        )
+        {/* (
           <div className="text-center my-24 text-lg">
             <div className="text-6xl mb-2">😵‍💫</div>Something wrong
           </div>
-        )}
+        )} */}
       </div>
       <footer className="absolute bottom-0 right-0 left-0 text-sm text-center pb-6 flex items-center justify-center">
         Powerd by{' '}
