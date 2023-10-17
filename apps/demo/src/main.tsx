595837b0b4d882f43778cd506c3c3dec13c9cfcf
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-10-17 23:55:35 +0900

    update sign conract page

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,10 +1,20 @@
+import { Routes } from '@generouted/react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Routes } from '@generouted/react-router'
+import { Client, Provider, cacheExchange, fetchExchange } from 'urql'
 import './index.css'
+import { GRAPHQL_API_ENDPOINT } from './utils/const.ts'
+
+// create client instance for GraphQL
+const client = new Client({
+  url: GRAPHQL_API_ENDPOINT,
+  exchanges: [cacheExchange, fetchExchange],
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Routes />
+    <Provider value={client}>
+      <Routes />
+    </Provider>
   </React.StrictMode>
 )
